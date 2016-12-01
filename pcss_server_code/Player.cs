637b1fd1845a5df6d02cbf57e8cc36207a75932c
RytmeAnders <aschristiansen@gmail.com>
2016-12-01 21:10:03 +0100

    Began Player class: Made writer/reader and disconnect

---
+++ pcss_server_code/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcss_server_code {
+
+    class Player {
+
+        public NetworkStream networkStream;
+        public StreamWriter streamWriter;
+        public StreamReader streamReader;
+        public Socket clientSocket;
+
+        public void Disconnect() {
+            streamReader.Close();
+            networkStream.Close();
+            streamWriter.Close();
+            clientSocket.Close();
+        }
+    }
+}
