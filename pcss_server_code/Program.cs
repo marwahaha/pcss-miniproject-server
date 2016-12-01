9990d2fb98de06820ff2f2ba517f3380ec1c5ec6
RytmeAnders <aschristiansen@gmail.com>
2016-12-01 14:08:09 +0100

    Resetting project after tests

--- pcss_server_code/Program.cs
+++ pcss_server_code/Program.cs
@@ -12,15 +12,11 @@ namespace pcss_server_code
     class Program
     {
         static TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 1234);
-        static bool isOnline;
-        static int hej = 0;
 
         static void Main()
         {
             tcpListener.Start();
             Console.WriteLine("Starting server...");
-            // fghjgfhfghfgh
-
          
         }
     }
