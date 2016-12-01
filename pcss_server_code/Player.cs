1558a083caef8ddac59ffbb64d9caee759bc7e40
RytmeAnders <aschristiansen@gmail.com>
2016-12-01 21:18:55 +0100

    Renamed it to player

---
+++ pcss_server_code/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcss_server_code {
+
+    class Player {
+
+        public NetworkStream networkStream;
+        public StreamWriter streamWriter;
+        public StreamReader streamReader;
+        public Socket clientSocket;
+
+        public void Disconnect() {
+            streamReader.Close();
+            networkStream.Close();
+            streamWriter.Close();
+            clientSocket.Close();
+        }
+    }
+}
